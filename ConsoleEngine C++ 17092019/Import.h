ff1d5d30b386981948eaf11b1ff126deec062064
Jonas hofmann <Jonash871j@gmail.com>
2020-10-01 13:49:19 +0200

    Add project files.

---
+++ ConsoleEngine C++ 17092019/Import.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "EngineCore.h"
+
+class Import
+{
+protected:
+	int width = 0;
+	int height = 0;
+
+protected:
+	char** characterArray;
+	char** colorArray;
+	char colorPaletteArray[16][3];
+	char** mapArray;
+
+protected:
+	void ImportACXSPR(std::string filename);
+	void ImportACCPAL(std::string filename);
+	void ImportMAP(std::string filename);
+};
+
---
+++ ConsoleEngine C++ 17092019/Main.cpp
@@ -0,0 +1,13 @@
+﻿#include "Main.h"
+
+void start()
+{
+	Console::Create(60, 60);
+	Console::SetFont(L"Terminal", 8, 8);
+	Console::SetColorPalette(cpDefault);
+}
+
+void update()
+{
+	Console::Update();
+}
